d66e666f7beaa545cdb66f3fdd8a5522a4fbea45
AryanMahajan <mahajanaryan0409@gmail.com>
2024-06-26 22:56:12 +0530

    Prodigy

--- path.py
+++ path.py
@@ -1,4 +1,4 @@
 
 
-TRAIN_PATH = r'C:\Users\DELL\Desktop\Internships\Prodigy InfoTech\PRODIGY_ML_01\house-prices-advanced-regression-techniques\train.csv'
-TEST_PATH = r'C:\Users\DELL\Desktop\Internships\Prodigy InfoTech\PRODIGY_ML_01\house-prices-advanced-regression-techniques\train.csv'
+CLEANED_TRAIN_PATH = r'C:\Users\DELL\Desktop\Internships\Prodigy InfoTech\PRODIGY_ML_01\house-prices-advanced-regression-techniques\train.csv'
+UNCLEANED_TEST_PATH = r'C:\Users\DELL\Desktop\Internships\Prodigy InfoTech\PRODIGY_ML_01\house-prices-advanced-regression-techniques\train.csv'
